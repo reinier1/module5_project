f55126dbb7c2c4ee2b7aeceb299d9e257f8e88c1
lotrbuilders <daan.2008@hotmail.com>
2020-10-27 15:02:45 +0100

    Update debug.vhd

--- vhdl bestanden/Debug/debug.vhd
+++ vhdl bestanden/Debug/debug.vhd
@@ -55,21 +55,21 @@ ARCHITECTURE salade OF debug IS
     END CASE;
   END hex2display;
 
-	SIGNAL dipswitches : std_logic_vector(7 DOWNTO 0);
+	SIGNAL dipswitches 					: std_logic_vector(7 DOWNTO 0);
 	SIGNAL instr_view_byte_on_address 	: std_logic := '0';
 	SIGNAL instr_write_byte_on_address 	: std_logic := '0';
 	SIGNAL instr_view_byte_next_address : std_logic := '0';
 	SIGNAL instr_write_byte_next_address: std_logic := '0';
 	SIGNAL instr_execute_and_wait 		: std_logic := '0';
-	SIGNAL byte				: std_logic_vector(7 DOWNTO 0);
-	SIGNAL counter_key2		: integer := 0;
-	SIGNAL part				: integer := 0;
-	SIGNAL byte_signal		: std_logic_vector(7 DOWNTO 0);
-	SIGNAL wait_for_byte	: integer := 5;
-	SIGNAL b_read_intern	: std_logic := '0';
-	SIGNAL b_write_intern	: std_logic := '0';
-	SIGNAL address_intern	: std_logic_vector(15 DOWNTO 0);
-	SIGNAL byte_out_intern	: std_logic_vector(7 DOWNTO 0);
+	SIGNAL byte							: std_logic_vector(7 DOWNTO 0);
+	SIGNAL counter_key2					: integer 	:= 0;
+	SIGNAL part							: integer 	:= 0;
+	SIGNAL byte_signal					: std_logic_vector(7 DOWNTO 0);
+	SIGNAL wait_for_byte				: integer 	:= 5;
+	SIGNAL b_read_intern				: std_logic := '0';
+	SIGNAL b_write_intern				: std_logic := '0';
+	SIGNAL address_intern				: std_logic_vector(15 DOWNTO 0);
+	SIGNAL byte_out_intern				: std_logic_vector(7 DOWNTO 0);
 
 BEGIN
 PROCESS(clk)
@@ -84,16 +84,16 @@ BEGIN
 		dipswitches(5) <= dip5;
 		dipswitches(6) <= dip6;
 		dipswitches(7) <= dip7;
-		wait_for_byte <= wait_for_byte+1;
+		wait_for_byte  <= wait_for_byte+1;
 		IF wait_for_byte = 2 THEN
 			IF b_read_intern = '1' THEN
-				byte_signal <= byte_in;
-				b_read <= '0';
-				b_read_intern <= '0';
+				byte_signal 	<= byte_in;
+				b_read 			<= '0';
+				b_read_intern 	<= '0';
 			ELSIF b_write_intern = '1' THEN
-				byte_signal <= byte_out_intern;
-				b_write <= '0';
-				b_write_intern <= '0';
+				byte_signal 	<= byte_out_intern;
+				b_write 		<= '0';
+				b_write_intern 	<= '0';
 			END IF;
 		END IF;
 		hex0 <= hex2display(byte_signal(3 DOWNTO 0));
@@ -104,15 +104,15 @@ BEGIN
 		hex5 <= hex2display(address_intern(15 DOWNTO 12));
 		IF (key3 = '0') THEN
 			IF dipswitches = "00000000" THEN
-				instr_view_byte_on_address <= '1';
+				instr_view_byte_on_address 		<= '1';
 			ELSIF dipswitches = "00000001" THEN
-				instr_write_byte_on_address <= '1';
+				instr_write_byte_on_address 	<= '1';
 			ELSIF dipswitches = "00000010" THEN
-				instr_view_byte_next_address <= '1';
+				instr_view_byte_next_address 	<= '1';
 			ELSIF dipswitches = "00000011" THEN
-				instr_write_byte_next_address <= '1';
+				instr_write_byte_next_address 	<= '1';
 			ELSIF dipswitches = "00000100" THEN
-				instr_execute_and_wait  <= '1';
+				instr_execute_and_wait  		<= '1';
 			END IF;
 		ELSIF key2 = '1' THEN
 			counter_key2 <= 0;
@@ -122,52 +122,52 @@ BEGIN
 				counter_key2 <= 0;
 				IF instr_view_byte_on_address = '1' THEN
 					IF part = 0 THEN
-						address(7 DOWNTO 0) <= dipswitches;
-						address_intern(7 DOWNTO 0) <= dipswitches;
-						part <= 1;
+						address(7 DOWNTO 0) 		<= dipswitches;
+						address_intern(7 DOWNTO 0) 	<= dipswitches;
+						part 						<= 1;
 					ELSIF part = 1 THEN
-						address(15 DOWNTO 8) <= dipswitches;
+						address(15 DOWNTO 8) 		<= dipswitches;
 						address_intern(15 DOWNTO 8) <= dipswitches;
-						b_read <= '1';
-						b_read_intern <= '1';
-						wait_for_byte <= 0;
-						instr_view_byte_on_address <= '0';
-						part <= 0;
+						b_read 						<= '1';
+						b_read_intern 				<= '1';
+						wait_for_byte 				<= 0;
+						instr_view_byte_on_address 	<= '0';
+						part 						<= 0;
 					END IF;
 				ELSIF instr_write_byte_on_address = '1' THEN	
 					IF part = 0 THEN
-						address(7 DOWNTO 0) <= dipswitches;
-						address_intern(7 DOWNTO 0) <= dipswitches;
-						part <= 1;
+						address(7 DOWNTO 0) 		<= dipswitches;
+						address_intern(7 DOWNTO 0) 	<= dipswitches;
+						part 						<= 1;
 					ELSIF part = 1 THEN
-						address(15 DOWNTO 8) <= dipswitches;
+						address(15 DOWNTO 8) 		<= dipswitches;
 						address_intern(15 DOWNTO 8) <= dipswitches;
-						part <= 2;
+						part 						<= 2;
 					ELSIF part = 2 THEN
-						byte_out <= dipswitches;
-						byte_out_intern <= dipswitches;
-						b_write <='1';
-						b_write_intern <= '1';
-						wait_for_byte <= 0;
+						byte_out 					<= dipswitches;
+						byte_out_intern 			<= dipswitches;
+						b_write 					<='1';
+						b_write_intern 				<= '1';
+						wait_for_byte 				<= 0;
 						instr_write_byte_on_address <= '0';
-						part <= 0;
+						part 						<= 0;
 					END IF;
 				ELSIF instr_view_byte_next_address = '1' THEN
-					address <= std_logic_vector(unsigned(address_intern) + to_unsigned(integer(4), address'length));
-					address_intern <= std_logic_vector(unsigned(address_intern) + to_unsigned(integer(4), address_intern'length));
-					b_read <= '1';
-					b_read_intern <= '1';
-					wait_for_byte <= 0;
+					address 		<= std_logic_vector(unsigned(address_intern) + to_unsigned(integer(4), address'length));
+					address_intern 	<= std_logic_vector(unsigned(address_intern) + to_unsigned(integer(4), address_intern'length));
+					b_read 			<= '1';
+					b_read_intern 	<= '1';
+					wait_for_byte 	<= 0;
 					instr_view_byte_next_address <= '0';
 				ELSIF instr_write_byte_next_address = '1' THEN
-					address <= std_logic_vector(unsigned(address_intern) + to_unsigned(integer(4), address'length));
-					address_intern <= std_logic_vector(unsigned(address_intern) + to_unsigned(integer(4), address_intern'length));
-					byte_out <= dipswitches;
+					address 		<= std_logic_vector(unsigned(address_intern) + to_unsigned(integer(4), address'length));
+					address_intern 	<= std_logic_vector(unsigned(address_intern) + to_unsigned(integer(4), address_intern'length));
+					byte_out 		<= dipswitches;
 					byte_out_intern <= dipswitches;
-					b_write <= '1';
-					b_write_intern <= '1';
-					wait_for_byte <= 0;
-					instr_write_byte_on_address <= '0';
+					b_write 		<= '1';
+					b_write_intern 	<= '1';
+					wait_for_byte 	<= 0;
+					instr_write_byte_on_address 	<= '0';
 				ELSIF instr_execute_and_wait = '1' THEN
 					null;
 					-- still to be made

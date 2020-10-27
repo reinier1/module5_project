6e0f989df8f3f86151d58159e74527286b699a5a
lotrbuilders <daan.2008@hotmail.com>
2020-10-27 10:00:52 +0100

    Refactor memory

--- vhdl bestanden/Memory/four-byte-enable.vhd
+++ vhdl bestanden/Memory/four-byte-enable.vhd
@@ -1,5 +1,6 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
 LIBRARY work;
 USE work.memory_package.ALL;
 ENTITY four_byte_enable_memory IS
@@ -11,15 +12,14 @@ ENTITY four_byte_enable_memory IS
 
 	PORT 
 	(
-		clk		: IN std_logic;
-		addr_a	: IN std_logic_vector(ADDR_WIDTH-1 DOWNTO 0);
-		addr_b	: IN std_logic_vector(ADDR_WIDTH-1 DOWNTO 0);
-		data_a	: IN std_logic_vector((DATA_WIDTH-1) DOWNTO 0);
-		--data_b: IN std_logic_vector((DATA_WIDTH-1) DOWNTO 0);
-		we_a	: IN std_logic := '1';
-		-- we_b	: IN std_logic := '1';
-		q_a		: OUT std_logic_vector((DATA_WIDTH -1) DOWNTO 0);
-		q_b		: OUT std_logic_vector((DATA_WIDTH -1) DOWNTO 0);
+		clk			: IN std_logic;
+		addr_a		: IN std_logic_vector(ADDR_WIDTH-1 DOWNTO 0);
+		addr_b		: IN std_logic_vector(ADDR_WIDTH-1 DOWNTO 0);
+		data_a		: IN std_logic_vector((DATA_WIDTH-1) DOWNTO 0);
+		we_a		: IN std_logic := '1';
+		
+		q_a			: OUT std_logic_vector((DATA_WIDTH -1) DOWNTO 0);
+		q_b			: OUT std_logic_vector((DATA_WIDTH -1) DOWNTO 0);
 		byte_enable	: IN std_logic_vector(3 DOWNTO 0)
 	);
 
@@ -27,22 +27,22 @@ END four_byte_enable_memory;
 
 ARCHITECTURE kerst OF four_byte_enable_memory IS
 	SIGNAL we_a_intern 	: std_logic_vector(3 DOWNTO 0); 
-	SIGNAL addr_a_i	: IN natural RANGE 0 to 2**ADDR_WIDTH - 1;
-	SIGNAL addr_b_i	: IN natural RANGE 0 to 2**ADDR_WIDTH - 1;
+	SIGNAL addr_a_i		: natural RANGE 0 to 2**ADDR_WIDTH - 1;
+	SIGNAL addr_b_i		: natural RANGE 0 to 2**ADDR_WIDTH - 1;
 BEGIN
 	addr_a_i <= to_integer(unsigned(addr_a));
 	addr_b_i <= to_integer(unsigned(addr_b));
+	
 	one_byte_enable_zero: ENTITY work.one_byte_enable_memory
 		GENERIC MAP ( RAM_INIT=>ROM0 )
 		PORT MAP 
 		(
 			clk	=> clk,
-			addr_a	=> addr_a,
-			addr_b	=> addr_b,
+			addr_a	=> addr_a_i,
+			addr_b	=> addr_b_i,
 			data_a	=> data_a(7 DOWNTO 0),
-			--data_b => data_b,
 			we_a	=> we_a_intern(0),
-			-- we_b	=> we_b,
+			
 			q_a		=> q_a(7 DOWNTO 0),
 			q_b		=> q_b(7 DOWNTO 0)
 		);
@@ -51,13 +51,12 @@ BEGIN
 		GENERIC MAP ( RAM_INIT=>ROM1 )
 		PORT MAP 
 		(
-			clk	=> clk,
-			addr_a	=> addr_a,
-			addr_b	=> addr_b,
+			clk		=> clk,
+			addr_a	=> addr_a_i,
+			addr_b	=> addr_b_i,
 			data_a	=> data_a(15 DOWNTO 8),
-			--data_b => data_b,
 			we_a	=> we_a_intern(1),
-			-- we_b	=> we_b,
+			
 			q_a		=> q_a(15 DOWNTO 8),
 			q_b		=> q_b(15 DOWNTO 8)
 		);
@@ -66,13 +65,12 @@ BEGIN
 		GENERIC MAP ( RAM_INIT=>ROM2 )
 		PORT MAP 
 		(
-			clk	=> clk,
-			addr_a	=> addr_a,
-			addr_b	=> addr_b,
+			clk		=> clk,
+			addr_a	=> addr_a_i,
+			addr_b	=> addr_b_i,
 			data_a	=> data_a(23 DOWNTO 16),
-			--data_b => data_b,
 			we_a	=> we_a_intern(2),
-			-- we_b	=> we_b,
+			
 			q_a		=> q_a(23 DOWNTO 16),
 			q_b		=> q_b(23 DOWNTO 16)
 		);
@@ -81,13 +79,12 @@ BEGIN
 		GENERIC MAP ( RAM_INIT=>ROM3 )
 		PORT MAP 
 		(
-			clk	=> clk,
-			addr_a	=> addr_a,
-			addr_b	=> addr_b,
+			clk		=> clk,
+			addr_a	=> addr_a_i,
+			addr_b	=> addr_b_i,
 			data_a	=> data_a(31 DOWNTO 24),
-			--data_b => data_b,
 			we_a	=> we_a_intern(3),
-			-- we_b	=> we_b,
+			
 			q_a		=> q_a(31 DOWNTO 24),
 			q_b		=> q_b(31 DOWNTO 24)
 		);
@@ -96,8 +93,8 @@ BEGIN
 	BEGIN
 		IF we_a ='1' THEN
 			we_a_intern <= byte_enable;
-		ELSE we_a_intern <= "0000";
+		ELSE 
+			we_a_intern <= "0000";
 		END IF;
-		
 	END PROCESS;
 END;
--- vhdl bestanden/Memory/memory_testbench.vhd
+++ vhdl bestanden/Memory/memory_testbench.vhd
@@ -13,23 +13,24 @@ ARCHITECTURE bhv OF testbench IS
 	SIGNAL reset 		: std_logic 								:= '0';
 	SIGNAL we_a			: std_logic 								:= '0';
 	SIGNAL byte_enable	: std_logic_vector(3 DOWNTO 0)				:= "1111";
-	SIGNAL addr_a		: std_logic_vector(ADDR_WIDTH-1 DOWNTO 0)	:= x"0000";
-	SIGNAL addr_b		: std_logic_vector(ADDR_WIDTH-1 DOWNTO 0)	:= x"0000";
+	SIGNAL addr_a		: std_logic_vector(16-1 DOWNTO 0)			:= x"0000";
+	SIGNAL addr_b		: std_logic_vector(16-1 DOWNTO 0)			:= x"0000";
 	SIGNAL data_a		: std_logic_vector((DATA_WIDTH-1) DOWNTO 0)	:= x"00000000";
 	SIGNAL q_a			: std_logic_vector((DATA_WIDTH -1) DOWNTO 0);
 	SIGNAL q_b			: std_logic_vector((DATA_WIDTH -1) DOWNTO 0);
 	
 	SIGNAL finished	: boolean 	:= FALSE;
 BEGIN
-	clk<= not clk AFTER 1 ms when not finished;
+	clk <= not clk AFTER 1 ms when not finished;
+	
 	mem: ENTITY work.four_byte_enable_memory
 		PORT MAP 
 		(
 			clk			=> clk,
 			we_a		=> we_a,
 			byte_enable	=> byte_enable,
-			addr_a		=> addr_a,
-			addr_b		=> addr_b,
+			addr_a		=> addr_a(ADDR_WIDTH-1 DOWNTO 0),
+			addr_b		=> addr_b(ADDR_WIDTH-1 DOWNTO 0),
 			data_a		=> data_a,
 			q_a			=> q_a,
 			q_b			=> q_b
@@ -37,32 +38,32 @@ BEGIN
 	PROCESS
 	BEGIN
 		WAIT FOR 10 ms;
-		reset<='1';
+		reset		<='1';
 		WAIT FOR 12 ms;
 		
-		we_a<='1';
-		data_a<=x"DEADBEEF";
+		we_a		<='1';
+		data_a		<=x"DEADBEEF";
 		WAIT FOR 4 ms;
-		we_a<='0';
-		WAIT FOR 4 ms;
-		
-		addr_a<=x"0013";
-		we_a<='1';
-		data_a<=x"f0f0f0f0";
-		WAIT FOR 4 ms;
-		we_a<='0';
-		WAIT FOR 4 ms;
-		addr_b<=x"0013";
+		we_a		<='0';
 		WAIT FOR 4 ms;
 		
-		addr_a<=x"0000";
-		we_a<='1';
-		byte_enable<="1010";
-		data_a<=x"f0f0f0f0";
+		addr_a		<=x"0013";
+		we_a		<='1';
+		data_a		<=x"f0f0f0f0";
 		WAIT FOR 4 ms;
-		we_a<='0';
+		we_a		<='0';
 		WAIT FOR 4 ms;
-		addr_b<=x"0000";
+		addr_b		<=x"0013";
+		WAIT FOR 4 ms;
+		
+		addr_a		<=x"0000";
+		we_a		<='1';
+		byte_enable	<="1010";
+		data_a		<=x"f0f0f0f0";
+		WAIT FOR 4 ms;
+		we_a		<='0';
+		WAIT FOR 4 ms;
+		addr_b		<=x"0000";
 		
 		WAIT FOR 10 ms;
 		ASSERT FALSE REPORT "done" SEVERITY note;

b7e99f431934744ccab90f42e252b83007283b86
Aukiee <72978516+Aukiee@users.noreply.github.com>
2020-10-29 17:09:08 +0100

    NOT WORKING testbench debug

--- vhdl bestanden/Debug/debug_testbench.vhd
+++ vhdl bestanden/Debug/debug_testbench.vhd
@@ -6,14 +6,7 @@ END debug_testbench;
 ARCHITECTURE koe_of_paard OF debug_testbench IS 
 	
 	SIGNAL clk 			: std_logic 								:= '0';
-	SIGNAL dip0			: std_logic 								:= '0';
-	SIGNAL dip1			: std_logic 								:= '0';
-	SIGNAL dip2			: std_logic 								:= '0';
-	SIGNAL dip3			: std_logic 								:= '0';
-	SIGNAL dip4			: std_logic 								:= '0';
-	SIGNAL dip5			: std_logic 								:= '0';
-	SIGNAL dip6			: std_logic 								:= '0';
-	SIGNAL dip7			: std_logic 								:= '0';
+	SIGNAL dipswithces_i: std_logic_vector(7 DOWNTO 0);
 	SIGNAL key1			: std_logic 								:= '1';
 	SIGNAL key2			: std_logic 								:= '1';
 	SIGNAL key3			: std_logic 								:= '1';
@@ -27,7 +20,9 @@ ARCHITECTURE koe_of_paard OF debug_testbench IS
 	SIGNAL hex2			: std_logic_vector(6 DOWNTO 0)	;		
 	SIGNAL hex3			: std_logic_vector(6 DOWNTO 0)	;			
 	SIGNAL hex4			: std_logic_vector(6 DOWNTO 0)	;			
-	SIGNAL hex5			: std_logic_vector(6 DOWNTO 0)	;			
+	SIGNAL hex5			: std_logic_vector(6 DOWNTO 0)	;	
+	SIGNAL debug_in		: std_logic;
+	SIGNAL debug_out	: std_logic;
 	
 	SIGNAL finished		: boolean 	:= FALSE;
 BEGIN
@@ -35,29 +30,24 @@ BEGIN
 	mem: ENTITY work.debug
 		PORT MAP 
 		(
-		clk		=> clk,
-		dip0	=> dip0,
-		dip1	=> dip1,
-		dip2	=> dip2,
-		dip3	=> dip3,
-		dip4	=> dip4,
-		dip5	=> dip5,
-		dip6	=> dip6,
-		dip7	=> dip7,
-		key1	=> key1,
-		key2	=> key2,
-		key3	=> key3,
-		byte_in	=> byte_in,
-		byte_out=> byte_out,
-		b_read	=> b_read,
-		b_write	=> b_write,
-		address	=> address,
-		hex0	=> hex0,
-		hex1	=> hex1,
-		hex2	=> hex2,
-		hex3	=> hex3,
-		hex4	=> hex4,
-		hex5	=> hex5
+		clk			=> clk,
+		dipswitches	=> dipswithces_i,
+		key1		=> key1,
+		key2		=> key2,
+		key3		=> key3,
+		byte_in		=> byte_in,
+		byte_out	=> byte_out,
+		b_read		=> b_read,
+		b_write		=> b_write,
+		address		=> address,
+		hex0		=> hex0,
+		hex1		=> hex1,
+		hex2		=> hex2,
+		hex3		=> hex3,
+		hex4		=> hex4,
+		hex5		=> hex5,
+		debug_in	=> debug_in,
+		debug_out	=> debug_out
 		);
 	PROCESS
 	BEGIN
@@ -66,29 +56,20 @@ BEGIN
 		WAIT FOR 10 ms;
 		key3 <= '1';
 		WAIT FOR 10 ms;
-		dip0 <= '1';
-		dip3 <= '1';
-		dip6 <= '1';
-		WAIT FOR 4 ms;
+		dipswithces_i <= "01001001"; 
+		WAIT FOR 10 ms;
 		key2 <= '0';
 		WAIT FOR 10 ms;
 		key2 <= '1';
-		dip0 <= '0';
-		dip3 <= '0';
-		dip6 <= '0';
-		dip1 <= '1';
-		dip2 <= '1';
-		dip4 <= '1';
+		dipswithces_i <= "00010110";
+
 		WAIT FOR 10 ms;
 		key2 <= '0';
 		WAIT FOR 10 ms;
 		key2 <= '1';
 		WAIT FOR 15 ms;
 		
-		dip1 <= '0';
-		dip2 <= '0';
-		dip4 <= '0';
-		dip1 <= '1';
+		dipswithces_i <= "00000010";
 		byte_in <= X"AD";
 		WAIT FOR 10 ms;
 		key3 <= '0';
@@ -100,55 +81,34 @@ BEGIN
 		key2 <= '1';
 		WAIT FOR 15 ms;
 		
-		dip1 <= '0';
-		dip0 <= '1';
+		dipswithces_i <= "00000001";
 		WAIT FOR 10 ms;
 		key3 <= '0';
 		WAIT FOR 10 ms;
 		key3 <= '1';
-		dip7 <= '1';
-		dip6 <= '1';
-		dip5 <= '1';
-		dip2 <= '1';
-		dip1 <= '1';
-		dip0 <= '1';
+		dipswithces_i <= "11100111";
 		WAIT FOR 10 ms;
 		key2 <= '0';
 		WAIT FOR 10 ms;
 		key2 <= '1';
-		dip7 <= '0';
-		dip6 <= '0';
-		dip5 <= '1';
-		dip2 <= '0';
-		dip1 <= '1';
-		dip0 <= '0';
+		dipswithces_i <= "00100010";
 		WAIT FOR 10 ms;
 		key2 <= '0';
 		WAIT FOR 10 ms;
 		key2 <= '1';
-		dip7 <= '1';
-		dip6 <= '0';
-		dip5 <= '0';
-		dip2 <= '0';
-		dip1 <= '0';
-		dip0 <= '1';
+		dipswithces_i <= "10000001";
 		WAIT FOR 10 ms;
 		key2 <= '0';
 		WAIT FOR 10 ms;
 		key2 <= '1';
 		WAIT FOR 15 ms;
 		
-		dip0 <= '1';
-		dip1 <= '1';
-		dip7 <= '0';
+		dipswithces_i <= "00000011";
 		WAIT FOR 10 ms;
 		key3 <= '0';
 		WAIT FOR 10 ms;
 		key3 <= '1';
-		dip4 <= '0';
-		dip6 <= '1';
-		dip5 <= '1';
-		dip2 <= '1';
+		dipswithces_i <= "01100100";
 		WAIT FOR 10 ms;
 		key2 <= '0';
 		WAIT FOR 10 ms;
